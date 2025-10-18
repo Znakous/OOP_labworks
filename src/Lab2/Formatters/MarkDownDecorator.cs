7ee7affe406a465ce227791172d2127aa35c1f8b
Vladislav Znak <znak.slava@mail.ru>
2025-10-18 14:11:13 +0300

    upd: markdown decorator

--- src/Lab2/Formatters/MarkDownDecorator.cs
+++ src/Lab2/Formatters/MarkDownDecorator.cs
@@ -1,10 +1,10 @@
 namespace Itmo.ObjectOrientedProgramming.Lab2.Formatters;
 
-public class MarkDownProxy : IFormatter
+public class MarkDownDecorator : IFormatter
 {
     private readonly IFormatter _formatter;
 
-    public MarkDownProxy(IFormatter formatter)
+    public MarkDownDecorator(IFormatter formatter)
     {
         _formatter = formatter;
     }
